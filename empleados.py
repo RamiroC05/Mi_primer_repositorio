a4e4e6a3dad1e5fc73e7472ff9d84d8d2956f171
RamiroCorra <corraramiro02@gmail.com>
2023-06-21 14:34:37 -0300

    actualizacion

--- empleados.py
+++ empleados.py
@@ -7,7 +7,7 @@ while resp != 4:
 
     print("\nMENU DE OPCIONES: ")
     print("1. Agregar empleados")
-    print("2. Leer lista de empleados")
+    print("2. Mostrar lista de empleados")
     print("3. Buscar empleados")
     print("4. Salir")
     resp = int(input("Seleccione una accion: "))
@@ -27,17 +27,20 @@ while resp != 4:
 
         print(texto)
         empleos.close()
-
+        
     if resp == 3:
         empleos= open('empleados.txt', 'r')
+        busqueda = input("Ingrese el nombre del empleado que desea buscar: ")
+        encontrado = False
         for x in empleos.readlines(): 
-            print (x)
-
-    
+            
+            if busqueda in x:
+                encontrado = True
+                break 
+        if encontrado:
+             print(f'El empleado {busqueda} se encuentra en la lista')
+        else:
+            print("El empleado no se encuentra en la lista")
+              
 
 print("Gracias por visitar el programa")
-
-
-
-
-
